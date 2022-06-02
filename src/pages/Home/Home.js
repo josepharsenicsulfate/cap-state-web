4d283dad9666cce42763ef76480f58f3fe494ed1
Joseph <josephgabriel.tina@outlook.com>
2022-06-02 13:54:46 +0800

    added listeners to form at home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,40 +5,69 @@ import './Home.css'
 import Navigation from '../../components/Navigation/Navigation'
 
 function Home() {
+  let inputSet = {
+    field1: '',
+    field2: '',
+    field3: '',
+    field4: ''
+  }
+
+  let inputGet
+
+  const change = (event) => {
+    event.preventDefault()
+    inputSet.field1 = document.querySelector('#input-1').value
+    inputSet.field2 = document.querySelector('#input-2').value
+    inputSet.field3 = document.querySelector('#input-3').value
+    inputSet.field4 = document.querySelector('#input-4').value
+    localStorage.setItem('input', JSON.stringify(inputSet))
+    alert('input submitted')
+    print()
+  }
+
+  const print = () => {
+    inputGet = JSON.parse(localStorage.getItem('input'))
+    document.querySelector('#field-1').innerHTML = inputGet.field1
+    document.querySelector('#field-2').innerHTML = inputGet.field2
+    document.querySelector('#field-3').innerHTML = inputGet.field3
+    document.querySelector('#field-4').innerHTML = inputGet.field4
+  }
+
   return (
     <div>
       <Navigation />
       <div className="home">
-        <section>
+        <section className='ccs-content'>
           <h2>Label</h2>
-          <h2>Lorem ipsum</h2>
+          <h2 id='field-1'>text</h2>
           <h2>Label</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p id='field-2'>text</p>
           <div className='grid'>
-            <div>
-              <ul>
-                <h2>Label</h2>
-                <li>Lorem</li>
-                <li>Ipsum</li>
-                <li>Dolor</li>
-              </ul>
-            </div>
-            <div>
-              <ul>
-                <h2>Label</h2>
-                <li>Lorem</li>
-                <li>Ipsum</li>
-                <li>Dolor</li>
-              </ul>
-            </div>
+            <ul>
+              <h2 id='field-3'>text</h2>
+              <li>Lorem</li>
+              <li>Ipsum</li>
+              <li>Dolor</li>
+            </ul>
+            <ul>
+              <h2 id='field-4'>text</h2>
+              <li>Lorem</li>
+              <li>Ipsum</li>
+              <li>Dolor</li>
+            </ul>
           </div>
         </section>
-        <section>
-          <form className='deets'>
-            <label htmlFor="">Label</label>
-            <input type="text" />
-            <label htmlFor="">Label</label>
-            <input type="text" />
+        <section className='ccs-form' >
+          <form className='deets' onSubmit={change}>
+            <label htmlFor=''>Label</label>
+            <input type='text' id='input-1' />
+            <label htmlFor=''>Label</label>
+            <input type='text' id='input-2' />
+            <label htmlFor=''>Label</label>
+            <input type='text' id='input-3' />
+            <label htmlFor=''>Label</label>
+            <input type='text' id='input-4' />
+            <button type='submit'>Save</button>
           </form>
         </section>
       </div>
